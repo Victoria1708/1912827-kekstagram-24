54d469cb91a2b465b7fe2f34afcf95f975ab2524
Victoria Kovtoniuk <va270554@gmail.com>
2021-09-27 20:06:04 +0300

    <fix> delete console.logs

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
 function getRandomInteger(from, to) {
+  document.querySelector('.error').innerHTML = '';
   from = Math.ceil(from);
   to = Math.floor(to);
   if (to < 0 || from < 0) {
-    console.log('Диапазон может быть только положительный, включая ноль');
+    document.querySelector('.error').innerHTML = 'Диапазон может быть только положительный, включая ноль';
     return;
   }
   if (to <= from) {
-    console.log('Значение «до» меньшее, чем значение «от», или равное ему');
+    document.querySelector('.error').innerHTML = 'Значение «до» меньшее, чем значение «от», или равное ему';
     return;
   }
   return Math.floor(Math.random() * (to - from + 1)) + from;
 }
 
-console.log(getRandomInteger(4,7));
-
 function isStringHasProperLength(string, maxLength) {
   return string.length < maxLength;
 }
 
-console.log(isStringHasProperLength('dshhdshdshu dhduusd', 150));
+getRandomInteger(2, 7);
+isStringHasProperLength('dshhdshdshu dhduusd', 150);
 
